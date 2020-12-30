2f7b04889ccc6b71950860e28c4c5ec5dec8f19f
Andrei Dumitrescu <andrei_dumitrescu2000@yahoo.ro>
2020-12-30 13:56:09 +0200

    Done admin panel

--- src/com/pachetepachete/AdminPanel.java
+++ src/com/pachetepachete/AdminPanel.java
@@ -11,28 +11,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
-public class AdminPanel extends JFrame implements ListSelectionListener {
+public class AdminPanel extends JFrame implements ListSelectionListener, ActionListener {
     JList<Company> companyJList;
     JList<User> userJList;
-    JList<Department> departmentJList;
-    JList<Job> jobJList;
-    JTextField author;
-    JTextField title;
+    JButton button;
     JTree jt;
     DefaultListModel<User> userDefaultListModel;
     DefaultListModel<Company> companyDefaultListModel;
-    DefaultListModel<Department> departmentDefaultListModel;
-    DefaultListModel<Job> jobDefaultListModel;
+    JPanel p;
+
 
     public AdminPanel() {
         super("Admin Panel");
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(500, 300));
+        setMinimumSize(new Dimension(700, 400));
         getContentPane().setBackground(Color.blue);
-        GridLayout gridLayout = new GridLayout(0, 3);
+        GridLayout gridLayout = new GridLayout(2, 1);
         gridLayout.setHgap(5);
         gridLayout.setVgap(5);
-        setLayout(gridLayout);
+        setLayout(new FlowLayout());
+
+        button = new JButton("Calculeaza salariile");
+        button.setEnabled(false);
+        button.addActionListener(this);
+        add(button);
+
+        GridLayout gridLayout1 = new GridLayout(0, 3);
+        gridLayout1.setHgap(5);
+        gridLayout1.setVgap(5);
+        p = new JPanel(gridLayout1);
 
 
         Application application = Application.getInstance();
@@ -45,7 +52,7 @@ public class AdminPanel extends JFrame implements ListSelectionListener {
         scrollPaneCompany.setViewportView(companyJList);
         companyJList.setLayoutOrientation(JList.VERTICAL);
         companyJList.addListSelectionListener(this);
-        add(scrollPaneCompany);
+        p.add(scrollPaneCompany);
 
         userDefaultListModel = new DefaultListModel<>();
         JScrollPane scrollPaneUser = new JScrollPane();
@@ -54,31 +61,13 @@ public class AdminPanel extends JFrame implements ListSelectionListener {
         userJList = new JList<>(userDefaultListModel);
         scrollPaneUser.setViewportView(userJList);
         userJList.setLayoutOrientation(JList.VERTICAL);
-        add(scrollPaneUser);
+        p.add(scrollPaneUser);
 
         DefaultMutableTreeNode node = new DefaultMutableTreeNode("Company");
         jt = new JTree(node);
-        add(jt);
-
-//        JPanel p = new JPanel(new BorderLayout());
-//        JPanel p2 = new JPanel(new BorderLayout());
-//        JPanel p3 = new JPanel(new BorderLayout());
-//        JLabel lb1 = new JLabel("Autor");
-//        JLabel lb2 = new JLabel("Titlu");
-//
-//        p2.add(lb1, BorderLayout.PAGE_START);
-//        p2.add(lb2, BorderLayout.PAGE_END);
-//
-//        author = new JTextField();
-//        author.setColumns(50);
-//        title = new JTextField();
-//        title.setColumns(50);
-//        p3.add(author,  BorderLayout.PAGE_START);
-//        p3.add(title, BorderLayout.PAGE_END);
-//
-//        p.add(p2, BorderLayout.LINE_START);
-//        p.add(p3, BorderLayout.LINE_END);
-//        add(p);
+        JScrollPane jScrollPane = new JScrollPane(jt);
+        p.add(jt);
+        add(p);
         show();
         pack();
     }
@@ -86,46 +75,73 @@ public class AdminPanel extends JFrame implements ListSelectionListener {
     public JTree getTree(Company company) {
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(company.getName());
         for (Department department : company.getDepartments()) {
-            DefaultMutableTreeNode dep = new DefaultMutableTreeNode(Application.getInstance().getDepartmentName(department));
-            DefaultMutableTreeNode people = new DefaultMutableTreeNode("Empoyees");
-            DefaultMutableTreeNode jobs = new DefaultMutableTreeNode("Jobs");
+            root.add(getDepNode(department));
+        }
 
-            for (Employee employee : department.getEmployees()) {
-                DefaultMutableTreeNode person = new DefaultMutableTreeNode(employee.getResume().getInformation().getFullname());
-                people.add(person);
-            }
+        return new JTree(root);
+    }
 
-            for (Job job : department.getJobs()) {
-                DefaultMutableTreeNode jb = new DefaultMutableTreeNode(job.getName());
-                jobs.add(jb);
-            }
+    public JTree getTreeWithBudget(Company company) {
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(company.getName());
+        for (Department department : company.getDepartments()) {
+            DefaultMutableTreeNode dep = getDepNode(department);
 
-            dep.add(people);
-            dep.add(jobs);
-
+            dep.add(new DefaultMutableTreeNode("Salarii totale: " + department.getTotalSalaryBudget()));
             root.add(dep);
         }
 
         return new JTree(root);
     }
 
+    private DefaultMutableTreeNode getDepNode(Department department) {
+        DefaultMutableTreeNode dep = new DefaultMutableTreeNode(Application.getInstance().getDepartmentName(department));
+        DefaultMutableTreeNode people = new DefaultMutableTreeNode("Empoyees");
+        DefaultMutableTreeNode jobs = new DefaultMutableTreeNode("Jobs");
+
+        for (Employee employee : department.getEmployees()) {
+            DefaultMutableTreeNode person = new DefaultMutableTreeNode(employee.getResume().getInformation().getFullname());
+            people.add(person);
+        }
+
+        for (Job job : department.getJobs()) {
+            DefaultMutableTreeNode jb = new DefaultMutableTreeNode(job.getName());
+            jobs.add(jb);
+        }
+
+        dep.add(people);
+        dep.add(jobs);
+
+        return dep;
+    }
+
     @Override
     public void valueChanged(ListSelectionEvent e) {
-        this.remove(jt);
+        p.remove(jt);
 
         if(companyJList.isSelectionEmpty()) {
             DefaultMutableTreeNode node = new DefaultMutableTreeNode("Company");
             jt = new JTree(node);
             jt.revalidate();
-            this.add(jt);
-            this.revalidate();
+            p.add(jt);
+            p.revalidate();
+            button.setEnabled(false);
             return;
         }
 
         Company company = companyJList.getSelectedValue();
         jt = getTree(company);
-        this.add(jt);
-        this.revalidate();
+        p.add(jt);
+        p.revalidate();
+        button.setEnabled(true);
     }
 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        p.remove(jt);
+        Company company = companyJList.getSelectedValue();
+        jt = getTreeWithBudget(company);
+        p.add(jt);
+        p.revalidate();
+    }
 }
