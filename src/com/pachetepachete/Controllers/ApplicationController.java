92d61a7e6831d42530532b402b8f6a22965ba0c9
Andrei Dumitrescu <andrei_dumitrescu2000@yahoo.ro>
2021-01-11 15:35:41 +0200

    connected panels

--- src/com/pachetepachete/Controllers/ApplicationController.java
+++ src/com/pachetepachete/Controllers/ApplicationController.java
@@ -7,24 +7,59 @@ import com.pachetepachete.Views.ProfilePage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ApplicationController extends JFrame {
+public class ApplicationController extends JFrame implements ActionListener {
+    private CardLayout layout;
+    private JPanel panelsSide;
+    private JPanel buttonsSide;
 
     public ApplicationController() {
         super("Admin Panel");
         setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(700, 400));
+        setMinimumSize(new Dimension(700, 700));
         getContentPane().setBackground(Color.blue);
-        setLayout(new CardLayout());
+        setLayout(new BorderLayout());
         AdminPanel adminPanel = new AdminPanel();
-        ManagerPage managerPage = new ManagerPage(Application.getInstance().getCompany("Amazon").getManager());
-        ProfilePage profilePage = new ProfilePage();
+        ManagerPage managerPanel = new ManagerPage(Application.getInstance().getCompany("Google").getManager());
+        ProfilePage profilePanel = new ProfilePage();
 
-        add(managerPage.getPanel(), "Manager Page");
-        add(adminPanel.getPanel(), "Admin Panel");
-        add(profilePage.getPanel(), "Profile Page");
+        layout = new CardLayout();
+        panelsSide = new JPanel(layout);
+
+        panelsSide.add(managerPanel.getPanel(), "1");
+        panelsSide.add(adminPanel.getPanel(), "2");
+        panelsSide.add(profilePanel.getPanel(), "3");
+
+        buttonsSide = new JPanel();
+        JButton adminPage = new JButton("Admin Page");
+        JButton managerPage = new JButton("Manager Page");
+        JButton profilePage = new JButton("Profile Page");
+
+        adminPage.addActionListener(this);
+        managerPage.addActionListener(this);
+        profilePage.addActionListener(this);
+
+        buttonsSide.add(managerPage);
+        buttonsSide.add(adminPage);
+        buttonsSide.add(profilePage);
+
+        getContentPane().add(panelsSide, BorderLayout.NORTH);
+        getContentPane().add(buttonsSide, BorderLayout.SOUTH);
 
         show();
         pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (((JButton) e.getSource()).getActionCommand().equals("Admin Page")) {
+            layout.show(panelsSide, "1");
+        } else if (((JButton) e.getSource()).getActionCommand().equals("Manager Page")) {
+            layout.show(panelsSide, "2");
+        } else if (((JButton) e.getSource()).getActionCommand().equals("Profile Page"))  {
+            layout.show(panelsSide, "3");
+        }
     }
 }
